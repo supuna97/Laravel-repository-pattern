64cc516c859b02cb7bfed138709773926173e8a8
Supun Nanayakkara <supunnanayakkara01@gmail.com>
2026-02-27 02:59:30 +0530

    Change return type to Collection.

--- my-app/app/Repositories/Api/V1/Interfaces/ItemRepositoryInterface.php
+++ my-app/app/Repositories/Api/V1/Interfaces/ItemRepositoryInterface.php
@@ -3,12 +3,13 @@
 namespace App\Repositories\Api\V1\Interfaces;
 
 use App\Models\Item;
+use Illuminate\Support\Collection;
 
 interface ItemRepositoryInterface
 {
     public function create(array $data): Item;
 
-    public function getAll(): array;
+    public function getAll(): Collection;
 
     public function getById(int $id): ?Item;
 
