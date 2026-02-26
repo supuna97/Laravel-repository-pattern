9c75d2b1f77e5508338892792d4b2373723ee244
Supun Nanayakkara <supunnanayakkara01@gmail.com>
2026-02-26 18:22:43 +0530

    Add api routings

--- my-app/bootstrap/app.php
+++ my-app/bootstrap/app.php
@@ -3,12 +3,20 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::middleware('api')
+                ->prefix('/v1')
+                ->namespace('App\\Http\\Controllers' . '\Api\V1')
+                // ->name('api.v1')
+                ->group(base_path('routes/api_v1.php'));
+        },
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
